15674fd1e8e9b61256c43f86fd6e16000b349daa
Razvan Migea <razvan.migea@target-e.com>
2019-10-31 01:15:59 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.ftsr1-1.fna.fbcdn.net/vp/ff88b10e0efa630ee918b7ff54650c94/5E5725D0/t51.2885-19/s320x320/31890461_2089463091329253_6622479196906061824_n.jpg?_nc_ht=instagram.ftsr1-1.fna.fbcdn.net" class="rounded-circle" style="max-height: 150px;">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>vava</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>52</strong> posts</div>
+                <div class="pr-5"><strong>358</strong> followers</div>
+                <div class="pr-5"><strong>207</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Răzvan Migea</div>
+            <div>
+                The biggest regret that people have on their deathbed is that they lived the life expected of them instead of a life true to themselves
+            </div>
+        </div>
+    </div>
+
+    <div class="row pt-4">
+        <div class="col-4">
+            <img alt="Image may contain: one or more people, people standing and outdoor" class="FFVAD" decoding="auto" sizes="293px" srcset="https://instagram.fclj2-1.fna.fbcdn.net/vp/8c1fc8edf69ef396df7855a20d6f3f8b/5E56C73F/t51.2885-15/e35/c0.135.1080.1080a/s150x150/73427509_790186374769538_3765583728809549164_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=102 150w,https://instagram.fclj2-1.fna.fbcdn.net/vp/a3b6325ce1439feb53ffd9840053486a/5E651575/t51.2885-15/e35/c0.135.1080.1080a/s240x240/73427509_790186374769538_3765583728809549164_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=102 240w,https://instagram.fclj2-1.fna.fbcdn.net/vp/9462d4cf008bf25d12b979670e560430/5E6101CF/t51.2885-15/e35/c0.135.1080.1080a/s320x320/73427509_790186374769538_3765583728809549164_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=102 320w,https://instagram.fclj2-1.fna.fbcdn.net/vp/b0b6c4981d12fd077e0621beff57a10e/5E45F695/t51.2885-15/e35/c0.135.1080.1080a/s480x480/73427509_790186374769538_3765583728809549164_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=102 480w,https://instagram.fclj2-1.fna.fbcdn.net/vp/7374a52f6a9e65344b4f1967726262da/5E5543F4/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/73427509_790186374769538_3765583728809549164_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=102 640w" src="https://instagram.fclj2-1.fna.fbcdn.net/vp/7374a52f6a9e65344b4f1967726262da/5E5543F4/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/73427509_790186374769538_3765583728809549164_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=102" style="object-fit: cover;" class="w-100">
+        </div>
+        <div class="col-4">
+            <img alt="Image may contain: one or more people, people standing, ocean, outdoor and water" class="FFVAD" decoding="auto" sizes="293px" srcset="https://instagram.fclj2-1.fna.fbcdn.net/vp/7a0e83fae8d6d5b20284d8bf75e2cfed/5E567777/t51.2885-15/e35/c82.0.915.915a/s150x150/65867893_148078363047873_8856991116388260783_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=109 150w,https://instagram.fclj2-1.fna.fbcdn.net/vp/1be373d96d5c32faa6a14448a80748f5/5E55323D/t51.2885-15/e35/c82.0.915.915a/s240x240/65867893_148078363047873_8856991116388260783_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=109 240w,https://instagram.fclj2-1.fna.fbcdn.net/vp/e7cfdcc1254fbb16b29c51ac55229478/5E604487/t51.2885-15/e35/c82.0.915.915a/s320x320/65867893_148078363047873_8856991116388260783_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=109 320w,https://instagram.fclj2-1.fna.fbcdn.net/vp/fd023c685c920343ff9bd3b93c44adea/5E59F2DD/t51.2885-15/e35/c82.0.915.915a/s480x480/65867893_148078363047873_8856991116388260783_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=109 480w,https://instagram.fclj2-1.fna.fbcdn.net/vp/55d30549a2150082e23cba1d1d5241f7/5E457D7D/t51.2885-15/sh0.08/e35/c82.0.915.915a/s640x640/65867893_148078363047873_8856991116388260783_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=109 640w" src="https://instagram.fclj2-1.fna.fbcdn.net/vp/55d30549a2150082e23cba1d1d5241f7/5E457D7D/t51.2885-15/sh0.08/e35/c82.0.915.915a/s640x640/65867893_148078363047873_8856991116388260783_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=109" style="object-fit: cover;">
+        </div>
+        <div class="col-4">
+            <img alt="Image may contain: one or more people, people standing, mountain, outdoor and nature" class="FFVAD" decoding="auto" sizes="293px" srcset="https://instagram.fclj2-1.fna.fbcdn.net/vp/166d398577029f2b04158a41d382d72c/5E59AE12/t51.2885-15/e35/c135.0.810.810a/s150x150/64910199_495713644516104_4097375483509970895_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=100 150w,https://instagram.fclj2-1.fna.fbcdn.net/vp/a77f87ab9787210c9663213da1d6d397/5E434B58/t51.2885-15/e35/c135.0.810.810a/s240x240/64910199_495713644516104_4097375483509970895_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=100 240w,https://instagram.fclj2-1.fna.fbcdn.net/vp/26d7c75df122519c97a88e1e8440b8e0/5E56D6E2/t51.2885-15/e35/c135.0.810.810a/s320x320/64910199_495713644516104_4097375483509970895_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=100 320w,https://instagram.fclj2-1.fna.fbcdn.net/vp/84206170a6f4bfea1f5773f11ce9a2aa/5E4047B8/t51.2885-15/e35/c135.0.810.810a/s480x480/64910199_495713644516104_4097375483509970895_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=100 480w,https://instagram.fclj2-1.fna.fbcdn.net/vp/cb88641ff22e70a89dd008d9fa7df95d/5E60E07A/t51.2885-15/sh0.08/e35/c135.0.810.810a/s640x640/64910199_495713644516104_4097375483509970895_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=100 640w" src="https://instagram.fclj2-1.fna.fbcdn.net/vp/cb88641ff22e70a89dd008d9fa7df95d/5E60E07A/t51.2885-15/sh0.08/e35/c135.0.810.810a/s640x640/64910199_495713644516104_4097375483509970895_n.jpg?_nc_ht=instagram.fclj2-1.fna.fbcdn.net&amp;_nc_cat=100" style="object-fit: cover;">
+        </div>
+    </div>
+</div>
+
+@endsection
